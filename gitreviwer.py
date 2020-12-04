8f6486e7b1bbe51c2b893b71b84aeca52cafdf00
cyghtinc <yaronil40@gmail.com>
2020-12-04 14:28:42 +0200

    'wasome'

--- gitreviwer.py
+++ gitreviwer.py
@@ -5,6 +5,13 @@ COMMIT_MESSAGE = 'comment from python script'
 
 import subprocess as cmd
 
+cp = cmd.run("git config --global user.name 'cyghtinc'", check=True, shell=True)
+
+
+cp = cmd.run("git config --global user.password 'git5git5'", check=True, shell=True)
+
+
 cp = cmd.run("git add .", check=True, shell=True)
 cp = cmd.run("git commit -m 'wasome'", check=True, shell=True)
 cp = cmd.run("git push", check=True, shell=True)
+
