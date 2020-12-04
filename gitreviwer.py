567cf85aa20bb1f9d46aff352814536ea24490f8
cyghtinc <yaronil40@gmail.com>
2020-12-04 14:35:36 +0200

    comment from python script

--- gitreviwer.py
+++ gitreviwer.py
@@ -1,15 +1,22 @@
 
-PATH_OF_GIT_REPO = r'https://github.com/cyghtinc/reviewdb.xlsx.git'  # make sure .git folder is properly configured
+GIT_REPO = r'.'  # make sure .git folder is properly configured
 COMMIT_MESSAGE = 'comment from python script'
 
-
+from git import Repo 
+"""
 import subprocess as cmd
 
-cp = cmd.run("git config user.name cyghtinc", check=True, shell=True)
-cp = cmd.run("git config user.password git5git5", check=True, shell=True)
+cp = cmd.run("git config --global user.name 'cyghtinc'", check=True, shell=True)
+cp = cmd.run("git config --global user.password 'git5git5'", check=True, shell=True)
 
 
 cp = cmd.run("git add .", check=True, shell=True)
 cp = cmd.run("git commit -m 'wasome'", check=True, shell=True)
 cp = cmd.run("git push", check=True, shell=True)
+"""
 
+repo = Repo(GIT_REPO)
+repo.git.add(update=True)
+repo.index.commit(COMMIT_MESSAGE)
+origin = repo.remote(name='origin')
+origin.push()
