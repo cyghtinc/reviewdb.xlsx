aacb88d039601078f89f7efa1a0e2181fb55a952
cyghtinc <yaronil40@gmail.com>
2020-12-05 19:21:14 +0200

    yes

--- gitreviwer.py
+++ gitreviwer.py
@@ -1,22 +1,19 @@
 
-GIT_REPO = r'C:\Users\Yaron Shamul\Documents\GitHub\reviewdb.xlsx'  # make sure .git folder is properly configured
-COMMIT_MESSAGE = 'comment from python script'
 
-from git import Repo 
-"""
-import subprocess as cmd
+from github import Github
+import pandas as pd  # openpyxl == 3.0.1
 
-cp = cmd.run("git config --global user.name 'cyghtinc'", check=True, shell=True)
-cp = cmd.run("git config --global user.password 'git5git5'", check=True, shell=True)
+#local_contents = dframe = pd.read_excel("reviewdb.xlsx")#pd.read_excel("reviewdb.xlsx", dtype=str)
 
+git_account = Github('cyghtinc', 'git5git5')
+git_account = Github('44078df37a1af38e33ccdaa94a9b5a32210ad913')
+repo = git_account.get_user().get_repo('reviewdb.xlsx')#('reviewdb.xlsx')
 
-cp = cmd.run("git add .", check=True, shell=True)
-cp = cmd.run("git commit -m 'wasome'", check=True, shell=True)
-cp = cmd.run("git push", check=True, shell=True)
-"""
-
-repo = Repo(GIT_REPO)
-repo.git.add(update=True)
-repo.index.commit(COMMIT_MESSAGE)
-origin = repo.remote(name='origin')
-origin.push()
+import git
+import subprocess
+subprocess.Popen("cd", cwd=r'C:\Users\Yaron Shamul\Documents\GitHub\reviewdb.xlsx') 
+# pull from remote origin to the current working dir:
+git.cmd.Git().pull('https://github.com/cyghtinc/reviewdb.xlsx','master')
+#reviewdb = repo.get_contents("reviewdb.xlsx")
+#print(reviewdb)
+#repo.update_file(reviewdb.path, "commit message", str(local_contents), reviewdb.sha)
